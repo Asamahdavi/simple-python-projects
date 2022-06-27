799228abeb2791f5496bdcc193023b14d575db23
Asamahdavi <asamahdavii@gmail.com>
2022-06-27 20:14:24 +0430

    add score system

--- quiz_game.py
+++ quiz_game.py
@@ -10,27 +10,33 @@ if playing.lower() !="yes":
 
 print("Greate! let's start the quiz :)")
 #print("Please enter with lowercase")
-
+score=0
 answer = input("What does CPU stands for? ")
 if answer.lower()  == "central proccessing unit":
     print("Correct! :)")
+    score+=1
 else:
     print("Incorrect! :(")
 
 answer = input("What does WWW stands for? ")
 if answer.lower()  == "world wide web":
     print("Correct! :)")
+    score+=1
 else:
     print("Incorrect! :(")
 
 answer = input("What does HTML stands for? ")
 if answer.lower()  == "hypertext markup language":
     print("Correct! :)")
+    score+=1
 else:
     print("Incorrect! :(")
 
 answer = input("What does CSS stands for? ")
 if answer.lower()  == "cascading style sheets":
     print("Correct! :)")
+    score+=1
 else:
     print("Incorrect! :(")
+ # becuase the last print input is string it is not okay to add string to number so we cast the score
+print("Your got "+ str((score/4)*100)+"%.")
